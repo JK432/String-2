6b00ff38abc9bf6043af2873b0117db2ab49c0d6
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-22 05:09:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Assume that the variable data contains the string “Python rules!”. 
+#A. Convert the string to uppercase
+#B. Locate the position of the string “rules”
+#C. Replace the exclamation point with a question mark
+x="Python rules!"
+print(x.upper())
+print(x.find("rules"))
+print(x.replace("rules!","Rules?"))
+
